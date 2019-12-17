3a6845aa5a1e56f248c8f45466ee0088934c1d45
Rouslan Gaisin <rouslan.gaisin@gmail.com>
2019-12-18 01:18:46 +0300

    add setup manifest

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""Setup manifest for Weekend Flights.
+
+NOTE:
+    To install or reintall the packages run `pip install -e .`
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="weekendflights",
+    description="Weekend Flights: cheap weekend flights search automated",
+    version="0.1",
+    author="Rouslan Gaisin",
+    author_email="rouslan.gaisin@gmail.com",
+    packages=find_packages(),
+)

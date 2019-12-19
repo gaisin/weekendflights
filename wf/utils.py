15ad4ce1a9603e44a393199e86b90c27d64d5e80
Rouslan Gaisin <rouslan.gaisin@gmail.com>
2019-12-20 00:30:41 +0300

    utils: add get_date_pairs()

--- wf/utils.py
+++ wf/utils.py
@@ -106,6 +106,65 @@ def get_next_wednesday(from_date=None):
     return today + diff
 
 
+def get_date_pairs(departure_date=None, arrival_date=None,
+                   on_weekends=False, next_x_months=None,
+                   trip_min_length=3, trip_max_length=7):
+    """Return all existing pairs of dates according to given conditions.
+
+    If on_weekends == True, return only weekend pairs - from thirsday to tuesday.
+    If on weekend == False, trip_max_length should be specified to return all available
+    pairs of dates on month from 'start_date' parameter.
+
+    Cases:
+    1) on_weekends=True, departure_date='2019-12-01', arrival_date='2020-02-28'
+        Func returns all existing pairs of dates for weekends
+        from departure date until arrival dates
+        in tuples with minimum three days of trip:
+            (2019-12-5, 2019-12-8),
+            (2019-12-5, 2019-12-9),
+            (2019-12-5, 2019-12-10),
+            (2019-12-6, 2019-12-9),
+            (2019-12-6, 2019-12-10),
+            (2019-12-7, 2019-12-10),
+            ...
+    TODO:
+    2) on_weekends=False, departure_date='2020-04-01', arrival_date='2020-05-06',
+       trip_min_length=7, trip_max_length=14
+        Func returns all existing pairs of dates from departure date until arrival dates
+        in tuples with minimum seven days of trip, and maximum 14 days of trip
+    """
+    if next_x_months is None:
+        departure_date = date(*[int(elem) for elem in departure_date.split('-')])
+        arrival_date = date(*[int(elem) for elem in arrival_date.split('-')])
+    else:
+        departure_date = date.today()
+        arrival_month = (departure_date.year, departure_date.month)
+        for i in range(next_x_months):
+            arrival_month = nextmonth(*arrival_month)
+        arrival_date = date(*arrival_month, 1)
+
+    result = []
+
+    one_day = timedelta(days=1)
+    while departure_date <= arrival_date:
+        week_day = WEEKDAYS[departure_date.weekday()]
+
+        if on_weekends:
+            if week_day == "Wednesday":
+                departure_date += one_day
+                continue
+
+            diff = timedelta(days=trip_min_length)
+            while departure_date + diff < get_next_wednesday(str(departure_date)):
+                date_pair = (str(departure_date), str(departure_date + diff))
+                result.append(date_pair)
+                diff += timedelta(days=1)
+
+        departure_date += one_day
+
+    return result
+
+
 def create_aviasales_link(origin, departure_date, destination, arrival_date):
     """"Creates link to aviasales flights serach on given params,
     and adds travelpayouts marker.

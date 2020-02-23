068905d36e6274609011e989b156f76c5193774b
Rouslan Gaisin <rouslan.gaisin@gmail.com>
2020-02-23 17:55:13 +0300

    add entry point for module installation

--- setup.py
+++ setup.py
@@ -13,4 +13,9 @@ setup(
     author="Rouslan Gaisin",
     author_email="rouslan.gaisin@gmail.com",
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "wf_find_flights = wf.find_flights:main",
+        ],
+    },
 )
